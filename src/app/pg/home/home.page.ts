49de3d3fba68f7134da387752778fe901ee56015
Patrick Library <patrickmatias.silva@hotmail.com>
2022-05-13 16:23:43 -0300

    slider de barbeiros finalizado e sliderFeed funcional

--- src/app/pg/home/home.page.ts
+++ src/app/pg/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+  slidesFeed = {
+    grabCursor: "true",
+    effect: 'creative',
+    creativeEffect:{ /* efeito criativo */
+      prev: {
+        shadow: true,
+        translate: [0, 0, -400]
+      },
+      next: {
+        translate: ['100%', 0, 0]
+      }
+    }, //fim efeito criativo
+    autoplay:{
+      delay: 2500,
+      disableOnInteraction: false
+    },
+    loop: true
+  };
+
+  slidesBarbeiro = {
+    loop: true,
+    autoplay:{
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    speed: 2500
+
+  }
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  
 }
