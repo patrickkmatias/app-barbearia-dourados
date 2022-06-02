9d3e749662c98cf63dcd6b6dd8aac3a8edd513b9
Patrick M <patrickmatias.silva@hotmail.com>
2022-06-02 09:47:03 -0300

    agora lista-barbeiros recebe dados do db

--- src/app/pg/lista-barbeiros/lista-barbeiros.page.ts
+++ src/app/pg/lista-barbeiros/lista-barbeiros.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UrlService } from '../../servidor/url.service';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-lista-barbeiros',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaBarbeirosPage implements OnInit {
 
-  constructor() { }
+  barbeiros:any
+
+  constructor(public servidorUrl: UrlService, public http: HttpClient) {
+    this.listarBarbeiros();
+   }
 
   ngOnInit() {
   }
+
+  listarBarbeiros() {
+
+    this.http.get(this.servidorUrl.pegarUrl() + 'lista-barbeiros.php')
+    .pipe(map(res => res))
+    .subscribe(listaDados =>{
+      this.barbeiros = listaDados
+    })
+
+  }
+
 
 }
