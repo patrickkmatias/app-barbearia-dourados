db87026ea4a33c8a9f7febd5f084c0599543419e
Patrick M <patrickmatias.silva@hotmail.com>
2022-06-14 18:02:05 -0300

    searchbar também na pg/local

--- src/app/pg/local/local.page.ts
+++ src/app/pg/local/local.page.ts
@@ -12,8 +12,20 @@ export class LocalPage implements OnInit {
 
   barbeiros:any
 
+  barbeiroItem: Array<{
+    idFuncionario: any,
+    nomeFuncionario: any,
+    repFuncionario: any
+  }>
+  barbeiroItemTodos: Array<{
+    idFuncionario: any,
+    nomeFuncionario: any,
+    repFuncionario: any
+  }>
+
   constructor(public servidorUrl: UrlService, public http: HttpClient) {
     this.listarItens();
+    this.barbeiroItem = [];
    }
 
   ngOnInit() {
@@ -25,7 +37,36 @@ export class LocalPage implements OnInit {
     .pipe(map(res => res))
     .subscribe(listaDados =>{
       this.barbeiros = listaDados
+
+      for (let i = 0; i < this.barbeiros.length; i++) {
+
+        this.barbeiroItem.push({
+          idFuncionario:listaDados[i]['idFuncionario'],
+          nomeFuncionario:listaDados[i]['nomeFuncionario'],
+          repFuncionario:listaDados[i]['repFuncionario']
+        });
+          
+        }
+        this.barbeiroItemTodos = this.barbeiroItem
+
     })
+
+  }
+
+  getItems(ev: any) {
+    const val = ev.target.value;
+
+    if (val && val.trim() != '') {
+      this.barbeiroItem = this.barbeiroItemTodos.filter((barbeiros) => {
+        return (barbeiros.nomeFuncionario.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      });
+      document.getElementById('semPreferencia').classList.add('display-none')
+
+    } else {
+      this.barbeiroItem = this.barbeiroItemTodos
+      document.getElementById('semPreferencia').classList.remove('display-none')
+    }
+
 
   }
 
