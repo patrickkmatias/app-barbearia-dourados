eaea64a64cba5709a8dbda8694bc8e64d65b9c0c
Patrick M <patrickmatias.silva@hotmail.com>
2022-06-14 18:22:15 -0300

    searchbar também em pg/agende

--- src/app/pg/agende/agende.page.ts
+++ src/app/pg/agende/agende.page.ts
@@ -12,9 +12,23 @@ export class AgendePage implements OnInit {
 
   servicos:any
 
+  servicoItem: Array<{
+    idServico: any,
+    nomeServico: any,
+    valorServico: any,
+    descricaoServico: any
+  }>
+  servicoItemTodos: Array<{
+    idServico: any,
+    nomeServico: any,
+    valorServico: any,
+    descricaoServico: any
+  }>
+
   constructor(public servidorUrl: UrlService, public http: HttpClient) { 
 
     this.listarServicos();
+    this.servicoItem = [];
   }
 
   ngOnInit() {
@@ -26,7 +40,34 @@ export class AgendePage implements OnInit {
     .pipe(map(res => res))
     .subscribe(listaDados => {
       this.servicos = listaDados
+
+      for (let i = 0; i < this.servicos.length; i++) {
+
+        this.servicoItem.push({
+          idServico:listaDados[i]['idServico'],
+          nomeServico:listaDados[i]['nomeServico'],
+          valorServico:listaDados[i]['valorComma'],
+          descricaoServico:listaDados[i]['descricaoServico']
+        });
+          
+        }
+        this.servicoItemTodos = this.servicoItem
+
     })
+  }
+
+  getItems(ev: any) {
+    const val = ev.target.value;
+
+    if (val && val.trim() != '') {
+      this.servicoItem = this.servicoItemTodos.filter((servicos) => {
+        return (servicos.nomeServico.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      });
+    } else {
+      this.servicoItem = this.servicoItemTodos
+    }
+
+
   }
 
 }
