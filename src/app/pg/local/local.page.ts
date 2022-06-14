11528221ca88f67cbc6fbe422734e515111c304b
Patrick M <patrickmatias.silva@hotmail.com>
2022-06-14 16:24:00 -0300

    função está funcionando, porém não declara a variavel 'vetor' (que receberia o .json) globalmente.

--- src/app/pg/local/local.page.ts
+++ src/app/pg/local/local.page.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { UrlService } from '../../servidor/url.service';
-import { map } from 'rxjs/operators';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-local',
@@ -12,21 +10,12 @@ export class LocalPage implements OnInit {
 
   barbeiros:any
 
-  constructor(public servidorUrl: UrlService, public http: HttpClient) {
-    this.listarItens();
+  constructor(public servidorUrl: UrlService) {
+    servidorUrl.conectarPhp('local.php', 'this.barbeiros');
    }
 
+
   ngOnInit() {
-  }
-
-  listarItens() {
-
-    this.http.get(this.servidorUrl.pegarUrl() + 'local.php')
-    .pipe(map(res => res))
-    .subscribe(listaDados =>{
-      this.barbeiros = listaDados
-    })
-
   }
 
 }
--- src/app/servidor/url.service.ts
+++ src/app/servidor/url.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -7,10 +9,27 @@ export class UrlService {
 
   url:string = "https://bdourados.000webhostapp.com/admin/"
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
 
   pegarUrl() {
     return this.url
   }
 
+  conectarPhp(arquivo: string, vetor: any) {  
+
+    console.log(vetor)
+
+    this.http.get(this.pegarUrl() + arquivo)
+    .pipe(map(res => res))
+    .subscribe(json =>{
+      vetor = json;
+      console.log(vetor)
+      
+    })
+
+    console.log(vetor)
+
+  }
+
+  
 }
