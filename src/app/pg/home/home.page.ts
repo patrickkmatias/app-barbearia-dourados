a0b9d7a6b5fff3826e2356cffeb45eef1988922a
Patrick M <patrickmatias.silva@hotmail.com>
2022-06-13 23:20:48 -0300

    página home trás seu próprio .php

--- src/app/pg/home/home.page.ts
+++ src/app/pg/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UrlService } from '../../servidor/url.service';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+
+  barbeiros:any;
 
   slidesFeed = {
     grabCursor: "true",
@@ -25,13 +29,23 @@ export class HomePage implements OnInit {
       disableOnInteraction: false
     },
     speed: 2500
-
   }
 
-  constructor() { }
+  constructor(public servidorUrl: UrlService, public http: HttpClient) {
+    this.listarBarbeiros();
+   }
 
   ngOnInit() {
   }
 
+  listarBarbeiros() {
+
+    this.http.get(this.servidorUrl.pegarUrl() + 'home.php')
+    .pipe(map(res => res))
+    .subscribe(listaDados =>{
+      this.barbeiros = listaDados
+    })
+
+  }
   
 }
