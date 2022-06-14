051bc374f38f9778b684333d7d6203925719856f
Patrick M <patrickmatias.silva@hotmail.com>
2022-06-14 15:37:58 -0300

    adicionado searchbar funcional em lista-barbeiros

--- src/app/pg/lista-barbeiros/lista-barbeiros.page.ts
+++ src/app/pg/lista-barbeiros/lista-barbeiros.page.ts
@@ -13,8 +13,20 @@ export class ListaBarbeirosPage implements OnInit {
 
   barbeiros:any
 
+  barbeiroItem: Array<{
+    idFuncionario: any,
+    nomeFuncionario: any,
+    repFuncionario: any
+  }>
+  barbeiroItemTodos: Array<{
+    idFuncionario: any,
+    nomeFuncionario: any,
+    repFuncionario: any
+  }>
+
   constructor(public servidorUrl: UrlService, public http: HttpClient) {
     this.listarBarbeiros();
+    this.barbeiroItem = [];
    }
 
   ngOnInit() {
@@ -26,7 +38,33 @@ export class ListaBarbeirosPage implements OnInit {
     .pipe(map(res => res))
     .subscribe(listaDados =>{
       this.barbeiros = listaDados
+
+      for (let i = 0; i < this.barbeiros.length; i++) {
+
+      this.barbeiroItem.push({
+        idFuncionario:listaDados[i]['idFuncionario'],
+        nomeFuncionario:listaDados[i]['nomeFuncionario'],
+        repFuncionario:listaDados[i]['repFuncionario']
+      });
+        
+      }
+      this.barbeiroItemTodos = this.barbeiroItem
+
     })
+
+  }
+
+  getItems(ev: any) {
+    const val = ev.target.value;
+
+    if (val && val.trim() != '') {
+      this.barbeiroItem = this.barbeiroItemTodos.filter((barbeiros) => {
+        return (barbeiros.nomeFuncionario.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      });
+    } else {
+      this.barbeiroItem = this.barbeiroItemTodos
+    }
+
 
   }
 
