2d19e5af4a2c0835fa3df3b2f34242bcc4abc789
Patrick M <patrickmatias.silva@hotmail.com>
2022-06-14 22:50:06 -0300

    requisição para login feita - funcionando

--- src/app/pg/login/login.page.ts
+++ src/app/pg/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UrlService } from '../../servidor/url.service';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  email:string;
+  senha:string;
+
+  constructor(public servidorUrl: UrlService, public http: HttpClient) {
+
+    this.email = "patrickmatias.silva@hotmail.com";
+    this.senha = "irineu";
+
+   }
 
   ngOnInit() {
+  }
+  
+  async logar(){
+    if(this.email == "" || this.senha == ""){
+      console.log("Preencha todos os campos.");
+      
+      this.servidorUrl.Alerta('Opa!','Preencha todos os campos.')
+
+    }else{
+
+      this.http.get(this.servidorUrl.pegarUrl() + 'login.php?email=' + this.email + '&senha=' + this.senha)
+      .pipe(map(rep => rep))
+      .subscribe(data => {
+        
+      })
+
+    }
   }
 
 }
--- src/app/servidor/url.service.ts
+++ src/app/servidor/url.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -7,10 +8,21 @@ export class UrlService {
 
   url:string = "https://bdourados.000webhostapp.com/admin/"
 
-  constructor() { }
+  constructor(public alerta: AlertController) { }
 
   pegarUrl() {
     return this.url
+  }
+
+  async Alerta(titulo, msg) {
+    const alert = await this.alerta.create({
+      //        cssClass: 'my-custom-class', podemos dar estilo ao alert
+                header: titulo,
+                message: msg,
+                buttons: ['OK']
+              });
+          
+              await alert.present();
   }
 
 }
