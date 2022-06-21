a536ce3512e99390a78e762a0f294e522f6b13f0
Patrick M <patrickmatias.silva@hotmail.com>
2022-06-21 18:41:13 -0300

    serviço a página cadastro feito, agora faz inserts no banco de dados.

--- src/app/pg/cadastro/cadastro.page.ts
+++ src/app/pg/cadastro/cadastro.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { UrlService } from '../../servidor/url.service';
+import { NavController } from '@ionic/angular';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -7,7 +12,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroPage implements OnInit {
 
-  constructor() { }
+  cadastro:any;
+  nome:any;
+  email:any;
+  senha:any;
+
+  constructor(public servidorUrl: UrlService, 
+              public http: HttpClient, 
+              public nav: NavController, 
+              public formConst: FormBuilder) { 
+
+                this.cadastro = this.formConst.group({
+                  nome: ['', Validators.required],
+                  email: ['', Validators.required],
+                  senha: ['', Validators.required]
+                })
+
+              }
+
+  cadCliente(){
+
+    if(this.nome == undefined || this.email == undefined || this.senha == undefined){
+
+      this.servidorUrl.Alerta('Atenção', 'Preencha todos os campos');
+
+    }else{
+
+      this.cadDados(this.cadastro.value).subscribe(
+
+        data => {
+          console.log('Teste de cadastro')
+        },
+        err => {
+          console.log('Erro de cadastro')
+        }
+
+      );
+
+    }
+
+  };
+
+  cadDados(dadosCad){
+
+    let cabecalho = new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded'});
+    return this.http.post(this.servidorUrl.pegarUrl()+'cadastro.php', dadosCad, {
+      headers: cabecalho
+    }).pipe(map((res) => { return res }));
+  };
 
   ngOnInit() {
   }
